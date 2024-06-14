5a5e65c54edbb992624ab9ae16c639a4a82a9447
Artrix9095 <sefu786@outlook.com>
2024-06-14 05:38:37 +0000

    Stack 🧠

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Nunito", ...fontFamily.sans],
+			},
+		},
+	},
+	plugins: [],
+};
